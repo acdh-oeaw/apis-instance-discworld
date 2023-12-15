ed31c2cfc922a90cf05d3ee61c6cdf68ca954697
Birger Schacht <birger.schacht@oeaw.ac.at>
2023-12-15 15:46:40 +0100

    fix: use default settings in settings file

--- discworld/settings.py
+++ discworld/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+from apis_acdhch_default_settings.settings import *
 
 from pathlib import Path
 
