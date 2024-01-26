d9e703a86dc486b53fbf900350f7d66e590a6d70
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-01-26 08:09:33 +0100

    fix: remove `apis_core.generic` from settings
    it is now part of the default settings
    
    [skip ci]

--- discworld/settings.py
+++ discworld/settings.py
@@ -28,5 +28,3 @@ LANGUAGE_CODE = 'en-us'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-INSTALLED_APPS += ["apis_core.generic"]
